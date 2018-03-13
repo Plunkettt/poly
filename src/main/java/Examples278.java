4d25befc9401f9f119afd873fe1a28f4e513a244
Ja <plukett@gmail.com>
2018-03-13 23:02:22 +0100

    first push

---
+++ src/main/java/Examples278.java
@@ -0,0 +1,19 @@
+/**
+ * @author Ja on 13/03/2018.
+ * @project poly,
+ */
+
+
+enum Note {
+    MIDDLE_C, C_SHARP, B_FLAT
+}
+
+class Instrument{
+    public void play(Note n){
+        System.out.println("Instrument.play()");
+    }
+}
+
+
+public class Examples278 {
+}
