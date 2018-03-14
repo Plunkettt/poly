56cfd33cb34a7b190d4e680b882d6a33e211c05d
Plunkettt <plukett@gmnail.com>
2018-03-14 15:40:33 +0100

    third commit

--- src/main/java/Examples278.java
+++ src/main/java/Examples278.java
@@ -20,6 +20,19 @@ class Wind extends Instrument {
     }
 }
 
+class Stringed extends Instrument{
+    public void play(Note n) {
+        System.out.println("Stringed.play() "+n);
+    }
+}
+
+class Brass extends Instrument{
+    public void play (Note n){
+        System.out.println("Brass.play() "+n);
+    }
+
+}
+
 class Music {
     public static void tune (Instrument i){
         i.play(Note.MIDDLE_C);
@@ -28,6 +41,39 @@ class Music {
     public static void main(String[] args) {
         Wind flute = new Wind();
         tune(flute);
+        Stringed violin = new Stringed();
+        tune(violin);
+    }
+}
+
+
+//Ex.1 pg.281
+
+
+class Cycle{
+    public void ride(){
+        System.out.println("riding");
+    }
+}
+
+class Unicycle extends Cycle{
+    Unicycle(){
+        System.out.println("AAAA");
+    }
+}
+
+class Bicycle extends Cycle{}
+
+class Tricycle extends Cycle{}
+
+class TheRide{
+    public static void ride2(Cycle c){}
+
+    public static void main(String[] args) {
+        Unicycle unicycle = new Unicycle();
+        unicycle.ride();
+
+        ride2(new Unicycle());
     }
 }
 
